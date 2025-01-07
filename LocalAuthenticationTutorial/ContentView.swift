336217047d1ca563accd51ddd936655cbd4422ae
Jayant Patil <jayant_patil1@epam.com>
2025-01-07 18:32:39 +0530

    Initial Commit

---
+++ LocalAuthenticationTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalAuthenticationTutorial
+//
+//  Created by Jayant Patil on 07/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalAuthenticationTutorial/LocalAuthenticationTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAuthenticationTutorialApp.swift
+//  LocalAuthenticationTutorial
+//
+//  Created by Jayant Patil on 07/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LocalAuthenticationTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
